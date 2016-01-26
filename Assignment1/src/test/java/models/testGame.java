4087834c7354bd8ec30cc4ac12d69e0070188809
Michael Hilton <michael.hilton@gmail.com>
2016-01-26 10:10:48 -0800

    added Game, Card, Suits with corresponding tests

---
+++ Assignment1/src/test/java/models/testGame.java
@@ -0,0 +1,45 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by michaelhilton on 1/25/16.
+ */
+public class testGame {
+
+    @Test
+    public void testGameCreation(){
+        Game g = new Game();
+        assertNotNull(g);
+    }
+
+    @Test
+    public void testGameBuildDeck(){
+        Game g = new Game();
+        g.buildDeck();
+        assertEquals(52,g.deck.size());
+    }
+
+    @Test
+    public void testGameInit(){
+        Game g = new Game();
+        g.buildDeck();
+        g.shuffle();
+        assertNotEquals(2,g.deck.get(0).getValue());
+    }
+
+    @Test
+    public void testGameStart(){
+        Game g = new Game();
+        g.buildDeck();
+        g.shuffle();
+        g.dealFour();
+        assertEquals(1,g.cols.get(0).size());
+        assertEquals(1,g.cols.get(1).size());
+        assertEquals(1,g.cols.get(2).size());
+        assertEquals(1,g.cols.get(3).size());
+    }
+
+}
