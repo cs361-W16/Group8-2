abba55909d874b0f64ab87c2e59bd9a741676745
Michael Hilton <michael.hilton@gmail.com>
2016-01-26 21:09:35 -0800

    deal is working back and forth

--- Assignment1/src/main/java/conf/Routes.java
+++ Assignment1/src/main/java/conf/Routes.java
@@ -30,7 +30,11 @@ public class Routes implements ApplicationRoutes {
         router.GET().route("/").with(ApplicationController.class, "index");
 //        router.GET().route("/hello_world.json").with(ApplicationController.class, "helloWorldJson");
         router.GET().route("/AcesUp").with(ApplicationController.class, "acesUp");
-        router.GET().route("/game.json").with(ApplicationController.class, "getGame");
+
+        router.GET().route("/game").with(ApplicationController.class, "gameGet");
+        router.POST().route("/dealGame").with(ApplicationController.class, "dealPost");
+        router.POST().route("/moveCard/{columnFrom}/{columnTo}").with(ApplicationController.class, "moveCard");
+
 
         ///////////////////////////////////////////////////////////////////////
         // Assets (pictures / javascript)
