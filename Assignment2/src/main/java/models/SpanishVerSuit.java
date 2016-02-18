759bb67adff32b255096060c9f8362dc702ac1ac
yeja94 <yeja@oregonstate.edu>
2016-02-17 21:39:35 -0800

    Spanish version suit
    Implemented the suits of coins, club, swords, cups

---
+++ Assignment2/src/main/java/models/SpanishVerSuit.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Jason Ye on 2/17/2016.
+ */
+public enum SpanishVerSuit implements Serializable {
+    Coins, Clubs, Cups, Swords
+}
