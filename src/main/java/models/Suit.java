7a26cbfd4b7a33955d32c3797d9152931ffa62ad
yeja94 <yeja@oregonstate.edu>
2016-02-18 19:20:06 -0800

    Revert "Spanish game and suits"
    This reverts commit 808f957634e2cd4a49dc4a64de4d1032b665664f.

--- src/main/java/models/Suit.java
+++ src/main/java/models/Suit.java
@@ -6,6 +6,6 @@ import java.io.Serializable;
  * Created by michaelhilton on 1/25/16.
  */
 public enum Suit  implements Serializable {
-    Hearts, Spades, Diamonds, Clubs, Coins, Swords, Cups
+    Hearts, Spades, Diamonds, Clubs
 }
 
