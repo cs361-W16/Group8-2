9cedacf64af5107737d3bb9b12b196baa5f0178d
yeja94 <yeja@oregonstate.edu>
2016-02-19 17:53:51 -0800

    Update Spanish_Game.java

--- src/main/java/models/Spanish_Game.java
+++ src/main/java/models/Spanish_Game.java
@@ -6,7 +6,7 @@ package models;
 public class Spanish_Game extends Game {
     public void buildSpanishDeck(){
         for (int i = 1; i < 11; i++){
-            deck.add(new Card(i, Suit.Cups));
+            deck.add(new Card(i,Suit.Cups));
             deck.add(new Card(i,Suit.Swords));
             deck.add(new Card(i,Suit.Coins));
             deck.add(new Card(i,Suit.Clubs));
