ef5c39c15be4007286ef5381830e6374f1a4bf57
yeja94 <yeja@oregonstate.edu>
2016-02-20 20:12:20 -0800

    Spanish version game

---
+++ src/main/java/models/Spanish_Game.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+/**
+ * Created by Jason Ye on 2/20/2016.
+ */
+public class Spanish_Game extends Game{
+    public void buildDeck(){
+        for (int i = 1; i < 11; i++){
+            deck.add(new Card(i,Suit.Coins));
+            deck.add(new Card(i,Suit.Cups));
+            deck.add(new Card(i,Suit.Swords));
+            deck.add(new Card(i,Suit.Clubs));
+        }
+    }
+}
--- src/main/java/models/Suit.java
+++ src/main/java/models/Suit.java
@@ -6,6 +6,6 @@ import java.io.Serializable;
  * Created by michaelhilton on 2/20/16.
  */
 public enum Suit  implements Serializable {
-    Hearts, Spades, Diamonds, Clubs, Coins, Swords, Cups 
+    Hearts, Spades, Diamonds, Clubs, Coins, Swords, Cups
 }
 
