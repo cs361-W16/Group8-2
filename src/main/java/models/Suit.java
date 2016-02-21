c0333be781f70928046b9daa69ca885551cc93ab
yeja94 <yeja@oregonstate.edu>
2016-02-20 19:56:56 -0800

    Update Suit.java

--- src/main/java/models/Suit.java
+++ src/main/java/models/Suit.java
@@ -3,7 +3,7 @@ package models;
 import java.io.Serializable;
 
 /**
- * Created by michaelhilton on 1/25/16.
+ * Created by michaelhilton on 2/20/16.
  */
 public enum Suit  implements Serializable {
     Hearts, Spades, Diamonds, Clubs, Coins, Swords, Cups 
